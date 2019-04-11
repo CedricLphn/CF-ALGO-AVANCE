ba90eae5fa5bfb9ab5be32d8ec6dd4f4f957f9d8
Flow2dot0 <florian.gustin.barriera@gmail.co>
2019-04-11 23:47:09 +0200

    solve

--- src/SolverInterface.php
+++ src/SolverInterface.php
@@ -1,5 +1,45 @@
 <?php
 
 interface SolverInterface {
-    public static function solve(SudokuGrid $grid, int $rowIndex, int $columnIndex): ?SudokuGrid;
+    public static function solve(SudokuGrid $grid, int $rowIndex = 0, int $columnIndex = 0): ?SudokuGrid 
+    {
+        $sudoku = $grid;
+
+        $loose = null;
+
+        $sudoku->isFilled();
+        $sudoku->isValid();
+
+        if (isFilled() == true)
+        {
+            $sudokuOrigin = $sudoku;
+
+            $random = random_int(1, 9);
+
+            $sudoku->isValueValidForPosition($rowIndex, $columnIndex, $random);
+
+            if (isValueValidForPosition() == false)
+            {
+                if ($rowIndex < 8)
+                {
+                    $sudoku->isValueValidForPosition($rowIndex, $columnIndex, $random);
+                }
+                elseif ($rowIndex == 8)
+                {
+                    $sudoku->isValueValidForPosition($rowIndex, $columnIndex, $random);
+                }
+            }
+            elseif (isValueValidForPosition() == true)
+            {
+                $sudoku->set($rowIndex, $columnIndex, $random);
+
+            }
+        }
+        elseif (isValid() == true)
+        {
+            return $loose;
+        }
+        
+        
+    }
 }
