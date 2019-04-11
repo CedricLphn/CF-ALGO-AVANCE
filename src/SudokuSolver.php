f80489db6a45c90b1a88eaada10159743d3139cf
Flow2dot0 <florian.gustin.barriera@gmail.co>
2019-04-11 23:50:17 +0200

    test

--- src/SudokuSolver.php
+++ src/SudokuSolver.php
@@ -3,4 +3,45 @@
 class SudokuSolver implements SolverInterface
 {
     /* Insérer le code ici */
+    public static function solve(SudokuGrid $grid, int $rowIndex = 0, int $columnIndex = 0): ?SudokuGrid 
+    {
+        $sudoku = $grid;
+
+        $loose = null;
+
+        $sudoku->isFilled();
+        $sudoku->isValid();
+
+        if (isFilled() == true)
+        {
+            $sudokuOrigin = $sudoku;
+
+            $random = random_int(1, 9);
+
+            $sudoku->isValueValidForPosition($rowIndex, $columnIndex, $random);
+
+            if (isValueValidForPosition() == false)
+            {
+                if ($rowIndex < 8)
+                {
+                    $sudoku->isValueValidForPosition($rowIndex, $columnIndex, $random);
+                }
+                elseif ($rowIndex == 8)
+                {
+                    $sudoku->isValueValidForPosition($rowIndex, $columnIndex, $random);
+                }
+            }
+            elseif (isValueValidForPosition() == true)
+            {
+                $sudoku->set($rowIndex, $columnIndex, $random);
+
+            }
+        }
+        elseif (isValid() == true)
+        {
+            return $loose;
+        }
+        
+        
+    }
 }
