82ccce27e4798258d8b58f87d86d62a6992f5d7a
Cédric <cedric.leprohon@yahoo.fr>
2019-04-11 10:47:41 +0200

    gitignore + construct

--- src/SudokuGrid.php
+++ src/SudokuGrid.php
@@ -2,5 +2,20 @@
 
 class SudokuGrid implements GridInterface
 {
+    private $data;
+
+    public static function loadFromFile($filepath): ?SudokuGrid {
+        if(!file_exists($filepath)) { return false; }
+
+        $file = fopen($filepath, "r");
+        
+        if(json_last_error() != JSON_ERROR_NONE) { return false; }
+        return new SudokuGrid(json_decode(stream_get_contents($file)));
+    }
+
+    public function __construct(array $data) {
+        $this->$data = $data;
+        return true;
+    }
     /* Insérer le code ici */
 }
